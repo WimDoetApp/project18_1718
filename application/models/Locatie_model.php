97c79608bc69b6bfb35d28a00805eb586df3357e
Wim Naudts <wimnaudts@yahoo.com>
2018-03-11 19:41:52 +0100

    github is ingewikkeld

--- application/models/Locatie_model.php
+++ application/models/Locatie_model.php
@@ -1,28 +1,2 @@
-<?php
-
-/**
- * Team 18 - Project APP 2APP-BIT - Thomas More
- */
-
-class Locatie_model extends CI_Model {
-    
-    function __construct()
-    {
-        parent::__construct();
-    }
-    
-    function get($id)
-    {
-        $this->db->where('id', $id);
-        $query = $this->db->get('personeelsfeest_locatie');
-        return $query->row();
-    }
-    
-    function getAllByNaam(){
-        $this->db->order_by('naam', 'asc');
-        $query = $this->db->get('personeelsfeest_locatie');
-        return $query->result();
-    }
-}
 
 
