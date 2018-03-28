52a0941c5553fe6fb9629d9fe060cc1029d2ef00
Wim Naudts <wimnaudts@yahoo.com>
2018-03-28 13:48:50 +0200

    Merging Locatie

--- application/controllers/Locatie.php
+++ application/controllers/Locatie.php
@@ -35,7 +35,7 @@ class Locatie extends CI_Controller {
         $this->load->model('locatie_model');
         $data['locaties'] = $this->locatie_model->getAll();
         
-        $partials = array('inhoud' => 'locatiebeheren/locatie_scherm', 'header' => 'main_header', 'footer' => 'main_footer');
+        $partials = array('inhoud' => 'LocatieBeheren/locatie_scherm', 'header' => 'main_header', 'footer' => 'main_footer');
         $this->template->load('main_master', $partials, $data);
     }
     
